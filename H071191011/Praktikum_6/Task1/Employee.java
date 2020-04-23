d222d1fd2bc25ebd9cff983fb1c0cddb5171887c
Miku AuahDark <auahdark687291@gmail.com>
2020-04-23 20:34:33 +0800

    Setengah dari praktikum 6

---
+++ H071191011/Praktikum_6/Task1/Employee.java
@@ -0,0 +1,21 @@
+// Pegawai
+
+class Employee
+{
+	public Employee(String name)
+	{
+		this.name = name;
+	}
+
+	public long getSalary()
+	{
+		return 2500000;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	protected String name;
+}
