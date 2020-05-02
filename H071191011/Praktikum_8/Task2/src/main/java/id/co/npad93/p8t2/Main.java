a8a9a0736a18552f99caa529325b2cd93eea9e3e
Miku AuahDark <auahdark687291@gmail.com>
2020-05-02 22:04:04 +0800

    Core Praktikum 8

---
+++ H071191011/Praktikum_8/Task2/src/main/java/id/co/npad93/p8t2/Main.java
@@ -0,0 +1,19 @@
+package id.co.npad93.p8t1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    public static void main(String[] a)
+    {
+		Application.launch(Main.class, a);
+	}
+	
+	@Override
+	public void start(Stage stage)
+	{
+		// GUI init code here
+		
+	}
+}
