47337f6a94e59979eb6bc1bbb227dcac9b5f2b50
Miku AuahDark <auahdark687291@gmail.com>
2020-05-03 23:21:13 +0800

    Task 3 done.
    Me after realizing the resulting Iofi livestream file broke: My disappointment is immeasurable and my day is ruined

--- H071191011/Praktikum_8/Task3/src/main/java/id/co/npad93/p8t3/Main.java
+++ H071191011/Praktikum_8/Task3/src/main/java/id/co/npad93/p8t3/Main.java
@@ -1,7 +1,13 @@
-package id.co.npad93.p8t1;
+package id.co.npad93.p8t3;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.io.*;
+
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
 public class Main extends Application
 {
@@ -13,7 +19,50 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage)
 	{
-		// GUI init code here
+		// Button
+		Button selectFileButton = new Button("Pilih Berkas");
+		selectFileButton.setScaleX(2);
+		selectFileButton.setScaleY(2);
+
+		// TextArea
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setPrefHeight(768);
+
+		// Button handler
+		selectFileButton.setOnAction(new FilePickerButtonEvent(selectFileButton, textArea, stage));
 		
+		// Root pane
+		VBox rootPane = new VBox();
+		VBox.setMargin(selectFileButton, new Insets(24, 16, 16, 16));
+		VBox.setMargin(textArea, new Insets(8, 8, 8, 8));
+		rootPane.setAlignment(Pos.TOP_CENTER);
+		rootPane.getChildren().addAll(selectFileButton, textArea);
+
+		Scene scene = new Scene(rootPane, 640, 480);
+		stage.setScene(scene);
+		stage.setTitle("File Reader");
+		stage.show();
+	}
+
+	static String readFile(File file) throws IOException
+	{
+		final int BUFSIZE = 4096;
+
+		FileInputStream input = new FileInputStream(file);
+		ByteArrayOutputStream tempOut = new ByteArrayOutputStream();
+
+		// Read file in chunks
+		byte[] buf = new byte[BUFSIZE];
+		int readed = 0;
+		do
+		{
+			readed = input.read(buf);
+			tempOut.write(buf, 0, readed);
+		} while (readed == BUFSIZE);
+
+		String out = tempOut.toString("UTF-8");
+		input.close();
+		return out;
 	}
 }
