007d04fbc012e15787ac51c7861a2675c0863f5c
Fauzi Asham <54959796+FauziAsham@users.noreply.github.com>
2020-06-03 23:22:27 +0800

    Praktikum-2

---
+++ Praktikum_2/BoxMain.java
@@ -0,0 +1,19 @@
+package Praktikum_2;
+
+import java.util.Scanner;
+
+public class BoxMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double width = input.nextDouble();
+        double height = input.nextDouble();
+        double depth = input.nextDouble();
+        double mass = input.nextDouble();
+        input.close();
+        Box box = new Box(width, height, depth);
+        box.setMass(mass);
+        System.out.println("Massa jenis = " + box.getDensity());
+        box.setMass(mass*2);
+        System.out.println("Massa jenis = " + box.getDensity());
+    }
+}
